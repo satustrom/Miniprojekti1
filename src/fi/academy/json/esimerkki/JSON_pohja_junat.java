7fc1f886f1318e250a350cee48577c96ccb6ed7b
PaulaSazonov <paula.sazonov@gmail.com>
2018-02-13 11:38:20 +0200

    Erillinen Juna-luokka lisätty

---
+++ src/fi/academy/json/esimerkki/JSON_pohja_junat.java
@@ -0,0 +1,59 @@
+package fi.academy.json.esimerkki;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/*
+Vaatii Jackson kirjaston:
+File | Project Structure
+Libraries >> Add >> Maven
+Etsi "jackson-databind", valitse versio 2.0.5
+Asentuu Jacksonin databind, sekä core ja annotations
+ */
+
+public class JSON_pohja_junat {
+
+    Juna juna = new Juna();
+
+    public static void main(String[] args) {
+        lueJunanJSONData();
+    }
+
+
+    private static void lueJunanJSONData() {
+
+        String baseurl = "https://rata.digitraffic.fi/api/v1";
+        try {
+            URL url = new URL(baseurl+"/live-trains/station/HKI/LH");
+            ObjectMapper mapper = new ObjectMapper();
+            CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
+            List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
+            //System.out.println(junat.get(0).getTrainNumber());
+            // Seuraavaa varten on toteutettava TimeTableRow luokka:
+            //System.out.println(junat.get(0).getTimeTableRows().get(0).getScheduledTime());
+            System.out.println("\n\n");
+            //System.out.println(junat.get(0));
+            for (int i = 0; i <11 ; i++) {
+                System.out.println(junat.get(i).perustiedot());
+            }
+
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+}
+
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class TimeTableRow {
+}
+
