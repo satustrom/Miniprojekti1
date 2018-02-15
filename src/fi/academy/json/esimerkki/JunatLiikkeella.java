081e85ae104ff1f989374673268cb4a136b3a0f7
saminykanen <nsami89@gmail.com>
2018-02-15 11:33:16 +0200

    JunatLiikkeella roska ei toimi

--- src/fi/academy/json/esimerkki/JunatLiikkeella.java
+++ src/fi/academy/json/esimerkki/JunatLiikkeella.java
@@ -11,37 +11,63 @@ import java.util.Collections;
 import java.util.List;
 
 public class JunatLiikkeella {
-    public static void liikkeella() {
+    public static void liikkeella(String lahtoasema, String maaraasema) {
+
+
+//Muutetaan käyttäjältä saatu kaupunki sitä vastaavaan lyhytkoodiin ja tallennetaan se muuttujaan.
+        String lAsema = Asemat.palautaLyhytkoodi(lahtoasema);
+        String kAsema = Asemat.palautaLyhytkoodi(maaraasema);
+
         String baseurl = "https://rata.digitraffic.fi/api/v1";
         String hakuehdot = "";
-        String hakuKaupunki = "";
 
         try {
-            URL url = new URL(baseurl+"/live-trains/"+hakuKaupunki+""+hakuehdot);
+//Syötetään hakuehdot URLiin
+            URL url = new URL(baseurl + "/live-trains/station/" + lAsema + "/" + kAsema + "?" + hakuehdot);
             ObjectMapper mapper = new ObjectMapper();
             CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
             List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
 
-          //  int tulostustenMaara = lkm;
-            for (int i = 0; i < junat.size() ; i++) {
-                String tulostus = "";
-                if (junat.get(i).isRunningCurrently()) {
-                    System.out.printf("Juna %s - %s \n\t Lähtenyt: %s\n\t Liikkeellä: %s\n\t Junan tyyppi: %s\n\t commuterlineID: %s\n"
-                            ,junat.get(i).getTrainType()
-                            ,junat.get(i).getTrainNumber()
-                            ,junat.get(i).getDepartureDate()
-                            ,junat.get(i).isRunningCurrently()
-                            ,junat.get(i).getTrainCategory()
-                            ,junat.get(i).getCommuterLineID());
+            System.out.println("Tulostetaan junat välillä: " + Asemat.palautaKaupunki(lAsema) + " - " + Asemat.palautaKaupunki(kAsema));
 
-                    System.out.println("----------------------------------------");
+            int i = 0;
+// Luodaan timetablerow lista
+            List<TimeTableRow> lista = junat.get(i).timeTableRows;
+// Luodaan apumuuttujat joilla saadaan aloitettua määrättyä minkä aseman aika tulostetaan ja miltä asemalta matka aloitetaan välipysäkkien tulostus
+            int alkuID = 0;
+            int loppuID = 0;
+            for (int j = 0; j < lista.size(); j++) {
+                if (lista.get(j).getStationShortCode().equals(lAsema))
+                    alkuID = j;
+                if (lista.get(j).getStationShortCode().equals(kAsema)) {
+                    loppuID = j;
+                    continue;
                 }
-
+            }
+// Tulostetaan Junan tiedot
+            for (int l = alkuID; l < loppuID; l++) {
+                List<TimeTableRow> lista2 = junat.get(l).timeTableRows;
+           //    if (junat.get(i).isRunningCurrently()) {
+                    System.out.println("----------------------------------------");
+                    System.out.printf("Juna %s - %s \n\t Lähtenyt:\t %s\n\t Asemalta:\t %s\n\t Määränpää:\t %s\n\t Juuri nyt:\t %s\n"
+                            , junat.get(l).getCommuterLineID()
+                            , junat.get(l).getTrainNumber()
+                            , lista2.get(0).haeAikaStringina()
+                            , Asemat.palautaKaupunki(lista2.get(0).getStationShortCode())
+                            , Asemat.palautaKaupunki(lista2.get(lista2.size()-1).getStationShortCode())
+                            , Asemat.palautaKaupunki(lista2.get(l).getStationShortCode()));
+                    System.out.println("----------------------------------------");
+          //  }
             }
 
         } catch (Exception ex) {
             System.out.println(ex);
         }
+    }
+
+
+    public static void main(String[] args) {
+        liikkeella("Helsinki", "Pasila");
     }
 
 }
