19f6c23d41f64bbfcf9e50eebcaf18fecd92c16c
unknown <saarinensamueli@gmail.com>
2018-02-15 15:48:56 +0200

    Luotu metodi IsoAlkukirjain, mikä muuttaa sanan alkaamaan isolla

---
+++ src/fi/academy/json/esimerkki/IsoAlkuKirjain.java
@@ -0,0 +1,21 @@
+package fi.academy.json.esimerkki;
+
+public class IsoAlkuKirjain {
+
+    public static StringBuilder alkukirjainmuotoilu;
+
+
+    public static String alkukirjainMuotoilu(String sana){
+        String uusiSana = sana.toLowerCase();
+
+        String alkuKirjain = uusiSana.substring(0, 1);
+
+        int pituus = uusiSana.length();
+
+        String valmis = alkuKirjain.toUpperCase()+uusiSana.substring(1,pituus);
+
+        return valmis;
+
+}
+}
+//metodi, joka muuttaa sanan alkamaan isolla kirjaimella ja muuten jatkumaan pienellä -Olli
